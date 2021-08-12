6458670b29decc53f6e84d7579565bf3c795e1b9
Your Name <you@example.com>
2021-08-11 21:42:50 -0300

    nuridiaz

--- dos.js
+++ dos.js
@@ -1,3 +1,70 @@
 function mostrar() {
-  alert("Ejercicio 2");
+ /*Realizar el algoritmo que permita ingresar los datos de los 500 alumnos de la UTN FRA, los datos solicitados son:
+nombre,
+carrera("Quimica";"Fisica";"Sistemas"),
+sexo (masculino - femenino -no binario),
+cantidad de materias(entre 1 y 5),
+Nota promedio del alumno(entre 0 y 10),
+edad (validar).
+Se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) El nombre del mejor promedio de los alumnos que estudian Fisica. (Sin importar el sexo).
+b) El nombre de la alumna mas joven.
+c) Porcentaje de estudiantes que estudia cada carrera.
+d) La edad y nombre del estudiante que cursa mas materias exceptuando la carrera de Quimica.*/
+
+
+
+let carrera;
+let sexo;
+let cantidadMaterias;
+let notaProm;
+let edad;
+let promFisica;
+
+nombre = prompt("Ingrese su nombre: ");
+
+carrera = prompt("Ingrese la carrera: ");
+while(carrera != "quimica" && carrera != "fisica" && carrera != "sistemas"){
+  carrera = prompt("Error. Ingrese la carrera correcta: ");
+}
+
+cantidadMaterias = prompt("Ingrese cantidad de materias: ");
+while(isNaN(cantidadMaterias) || cantidadMaterias < 1 || cantidadMaterias > 5){
+  cantidadMaterias = prompt("Error. Ingrese cantidad de materias: ");
+}
+
+sexo = prompt("Ingrese su sexo: ");
+  while(sexo != "masculino" || sexo != "femenino" || sexo != "no binario"){
+sexo = prompt("Error. Ingrese su sexo: ");
+}
+
+edad = parseInt(prompt("Ingrese edad: "));
+while(isNaN(edad) || edad <= 0 ){
+  edad = parseInt(prompt("Error. Ingrese edad: "));
+}
+
+nota = parseInt(prompt("Ingrese nota: "));
+while(isNaN(nota) || nota < 0 || nota > 10){
+  nota = parseInt(prompt("Error. Ingrese nota: "));
+}
+
+a) El nombre del mejor promedio de los alumnos que estudian Fisica.
+
+if()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- uno.js
+++ uno.js
@@ -1,3 +1,137 @@
 function mostrar() {
-  alert("Ejercicio 1");
+  
+/*Se necesita llevar la gestión de una veterinaria. Para ello se podrán registrar los datos de cada mascota mientras el usuario quiera.
+De cada mascota se solicita:
+-nombre (entre 3 y 8 caracteres)
+-tipo (“mamifero”, “ave”, “reptil”)
+-sangre ( "fria", "calida")
+Si es de tipo reptil solo puede tener sangre fria
+-edad ( mayor a cero)
+-sexo( “m” de macho o “h” de hembra )
+Informar:
+
+a- Promedio de edad de los reptiles
+b- tipo y sexo de la mascota mas joven
+c- Del total de mascotas que porcentaje son aves
+d- De que tipo de mascota hay mas cantidad*/
+
+
+//promedio, acum y cont para cada reptil
+//contMascMasJoven, flagMascMasJoven,
+//porcAves, contAves,
+//comparar cantidades de tipos de mascotas
+
+
+let nombre;
+let tipo;
+let sangre;
+let edad;
+let sexo;
+let promedioReptil=0;
+let acumReptil=0;
+let contReptil=0;
+let flagReptil=1;
+let flagMascMasJoven=1;
+let totalMascotas;
+let porcAves;
+let contAves=0;
+let contMamifero=0;
+let seguir;
+let edadMascMasJoven;
+let sexoMascMasJoven;
+let tipoMascMasJoven;
+let acumAves=0;
+let acumMamifero=0;
+let masMascotas;
+
+do{
+
+nombre = prompt("Ingrese el nombre de la mascota: ");
+while(nombre.length < 3 || nombre.length > 8){
+  nombre = prompt("Error. Ingrese el nombre de la mascota: ");
 }
+
+tipo = prompt("Ingrese el tipo de mascota: ");
+while(tipo != "mamifero" && tipo != "ave" && tipo != "reptil"){
+  tipo = prompt("Error. Ingrese el tipo de mascota: ");
+}
+
+sangre = prompt("Ingrese el tipo de sangre: ");
+while(sangre != "fria" && sangre != "calida"){
+  sangre = prompt("Error. Ingrese el tipo de sangre: ");
+}
+
+edad = parseInt(prompt("Ingrese edad: "));
+while(isNaN(edad) || edad <= 0 ){
+  edad = parseInt(prompt("Error. Ingrese edad: "));
+}
+
+sexo = prompt("Ingrese su sexo: ");
+  while(sexo != "m" && sexo != "h"){
+}sexo = prompt("Error. Ingrese su sexo: ");
+
+
+  if(flagReptil){
+  acumReptil += tipo;
+  contReptil++;
+  flagReptil == 0;
+  }
+ 
+switch(tipo){
+  case "reptil":
+    while(sangre != "fria"){
+      alert("Error. Solos los reptiles pueden tener sangre fria");
+    }
+    acumReptil += tipo;
+    contReptil++;
+  break;
+
+  case "ave":
+    acumAves += tipo;
+    contAves++;
+  break;
+
+  case "mamifero":
+   acumMamifero += tipo;
+   contMamifero++;
+   break;
+}
+
+if(flagMascMasJoven){
+  edadMascMasJoven = edad;
+  tipoMascMasJoven = tipo;
+  sexoMascMasJoven = sexo;
+ flagMascMasJoven == 0;
+}
+
+totalMascotas = acumReptil + acumMamifero + acumAves;
+porcAves = (contAves/totalMascotas) * 100;
+
+seguir = prompt("Quiere ingresar otra mascota?");
+}while(seguir == "s");
+
+if(acumReptil != 0){
+promedioReptil = acumReptil/contReptil;
+}else(acumReptil == 0)
+
+
+
+if( acumReptil > acumMamifero && acumReptil > acumAve){
+ masMascotas = "reptil";
+}else if(acumMamifero >= acumReptil && acumMamifero > acumAve){
+  masMascotas = "mamifero";
+}else{
+  masMascotas = "ave";
+}
+
+document.write("a)Promedio de edad de los reptiles: " + promedioReptil + "<br>");
+document.write("b)Tipo de la mascota mas joven: " + tipo + " Sexo de la mascota mas joven " + sexo + "<br>");
+document.write("c)Porcentaje de aves:  " + porcAves + "<br>");
+document.write("d)Tipo de mascota con mas cantidad:  " + masMascotas + "<br>");
+
+
+}
+
+
+
+
